ed81e8c7a8b3e0202bf37435af4471dec673c032
woomy144 <73113918+woomy144@users.noreply.github.com>
2020-11-07 22:35:22 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4648,7 +4648,7 @@ var maintainloop = (() => {
         };
     })();
     let spawnCrasher = census => {
-        if (ran.chance(1 -  0.5 * census.crasher / room.maxFood / room.nestFoodAmount)) {
+        if (ran.chance(1 -  0.5 * census.crasher / room.nestFoodAmount)) {
             let spot, i = 30;
             do { spot = room.randomType('nest'); i--; if (!i) return 0; } while (dirtyCheck(spot, 100));
             let type = (ran.dice(80)) ? ran.choose([Class.sentryGun, Class.sentrySwarm, Class.sentryTrap, exports.grushsentry]) : Class.crasher;
